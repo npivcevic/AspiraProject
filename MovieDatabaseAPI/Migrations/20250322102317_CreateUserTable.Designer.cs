1b35cc9f7e00b8934f6fa61bf106a47d7adba24f
Nikola Pivcevic <nikolapivcevic@yahoo.com>
2025-03-22 11:23:53 +0100

    CreateUserTable migration
    Created in Package Manager Console by running 'Add-Migration CreateUserTable'

--- MovieDatabaseAPI/Migrations/20250322102317_CreateUserTable.Designer.cs
+++ MovieDatabaseAPI/Migrations/20250322102317_CreateUserTable.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MovieDatabaseAPI;
 
@@ -10,9 +11,11 @@ using MovieDatabaseAPI;
 namespace MovieDatabaseAPI.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20250322102317_CreateUserTable")]
+    partial class CreateUserTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
