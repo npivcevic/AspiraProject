d06029072cbc2409d19c2d111a2c7fb5d7f83231
Nikola Pivcevic <nikolapivcevic@yahoo.com>
2025-03-23 16:12:26 +0100

    Add Reviews entity (with 1-n relationships), migration, controller, DTOs, Mapper

---
+++ MovieDatabaseAPI/DTOs/ReviewDto.cs
@@ -0,0 +1,47 @@
+using System.ComponentModel.DataAnnotations;
+using MovieDatabaseAPI.Models;
+
+namespace MovieDatabaseAPI.DTOs;
+
+public class ReviewDto
+{
+    public int Id { get; set; }
+    public string Content { get; set; } = "";
+    public int Rating { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime? UpdatedAt { get; set; }
+
+    public required UserListDto User { get; set; }
+    public required MovieListDto Movie { get; set; }
+}
+
+public class ReviewListDto
+{
+    public int Id { get; set; }
+    public int Rating { get; set; }
+    public DateTime CreatedAt { get; set; }
+
+    public required UserListDto User { get; set; }
+    public required MovieListDto Movie { get; set; }
+}
+
+public class ReviewCreateDto
+{
+    [StringLength(1000)]
+    public string Content { get; set; } = "";
+    [Range(1, 10)]
+    public int Rating { get; set; }
+    public int UserId { get; set; }
+    public int MovieId { get; set; }
+}
+
+public class ReviewUpdateDto
+{
+    public int Id { get; set; }
+    [StringLength(1000)]
+    public string Content { get; set; } = "";
+    [Range(1, 10)]
+    public int Rating { get; set; }
+    public int UserId { get; set; }
+    public int MovieId { get; set; }
+}
---
+++ MovieDatabaseAPI/Migrations/20250323150340_CreateReviewsTable.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MovieDatabaseAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateReviewsTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Reviews",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Content = table.Column<string>(type: "nvarchar(1000)", maxLength: 1000, nullable: false),
+                    Rating = table.Column<int>(type: "int", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    MovieId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Reviews", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Reviews_Movies_MovieId",
+                        column: x => x.MovieId,
+                        principalTable: "Movies",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Reviews_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reviews_MovieId",
+                table: "Reviews",
+                column: "MovieId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reviews_UserId",
+                table: "Reviews",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Reviews");
+        }
+    }
+}
