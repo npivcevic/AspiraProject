410eb14adf99c8dee2bc6bd4fa9e39dc27395126
Nikola Pivcevic <nikolapivcevic@yahoo.com>
2025-03-23 11:06:44 +0100

    Add Data Annotations on User DTOs for input validation

--- MovieDatabaseAPI/DTOs/UserDto.cs
+++ MovieDatabaseAPI/DTOs/UserDto.cs
@@ -19,17 +19,27 @@ public class UserListDto
 
 public class UserCreateDto
 {
+    [EmailAddress]
+    [StringLength(100)]
     public required string Email { get; set; }
+    [StringLength(100)]
     public required string Password { get; set; }
+    [StringLength(100)]
     public string FirstName { get; set; } = "";
+    [StringLength(100)]
     public string LastName { get; set; } = "";
 }
 
 public class UserUpdateDto
 {
     public int Id { get; set; }
+    [EmailAddress]
+    [StringLength(100)]
     public required string Email { get; set; }
+    [StringLength(100)]
     public required string Password { get; set; }
+    [StringLength(100)]
     public string FirstName { get; set; } = "";
+    [StringLength(100)]
     public string LastName { get; set; } = "";
 }
